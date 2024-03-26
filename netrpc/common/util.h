022691b0e1cb4a88624f659ded404357723cdacb
byrantz <imzhouwm@163.com>
2024-03-26 15:53:58 +0800

    fix threadgroup

--- netrpc/common/util.h
+++ netrpc/common/util.h
@@ -14,10 +14,12 @@ int64_t getNowMs();
 
 // 网络字节序转换为主机字节序
 int32_t getInt32FromNetByte(const char* buf);
-}
 
 double getCPUUtilization();     // 获取CPU利用率
 
 double getAverageLoad();        // 获取系统的平均负载能力
+
+}
+
 
 #endif
